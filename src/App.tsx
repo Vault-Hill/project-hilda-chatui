677d337045fc3ac176ebec4f9e05fd3adda12427
praise159praise <praisebamiyo159@gmail.com>
2023-09-29 15:08:14 +0000

    feat added transitional pae for loading and page for  bad connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
-import { useAtom } from 'jotai';
-import Main from './components/Main';
-import Navbar from './components/Navbar';
-import { connectionAtom, createSocketAtom } from './state/atoms';
-
+import { useAtom } from "jotai";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import { connectionAtom, createSocketAtom } from "./state/atoms";
+import { BallTriangle, Vortex } from "react-loader-spinner";
 
 function App() {
   const [, createConnection] = useAtom(createSocketAtom);
   createConnection();
-  const [{ connected, agentName, logoUrl }] = useAtom(connectionAtom);
+  const [{ connected, agentName, timedOut, logoUrl }] = useAtom(connectionAtom);
 
-  if (!connected) {
-    return <p>Dear Taiwo, A Cool transition/loading page will be nice here</p>;
+  if (!connected && !timedOut) {
+    console.log(timedOut);
+    return (
+      <div className="fixed w-[100vw] h-[100vh] bg-black flex gap-10 justify-center items-center text-white text-2xl">
+        <BallTriangle
+          height={50}
+          width={50}
+          radius={5}
+          color="#FFDA4C"
+          ariaLabel="ball-triangle-loading"
+          visible={true}
+        />
+        <p>Please wait while Hilda is booting up</p>
+      </div>
+    );
+  }
+  if (timedOut == true) {
+    return (
+      <div className="fixed w-[100vw] h-[100vh] bg-black flex gap-10 justify-center items-center text-white text-2xl">
+        <Vortex
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="vortex-loading"
+          wrapperStyle={{}}
+          wrapperClass="vortex-wrapper"
+          colors={["#ffffff40", "#FFDA4C","#ffffff40", "#FFDA4C","#ffffff40", "#FFDA4C" ]}
+        />
+        <p>Please check your internet connection</p>
+      </div>
+    );
   }
 
   return (
-    <div className='h-screen bg-slate-50 dark:bg-black flex flex-col items-center'>
+    <div className="h-screen bg-slate-50 dark:bg-black flex flex-col items-center">
       <Navbar agentName={agentName!} />
       <Main logoUrl={logoUrl} agentName={agentName!} />
     </div>
