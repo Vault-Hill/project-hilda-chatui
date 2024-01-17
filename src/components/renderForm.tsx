33d7b5a482c424d1f38c91c780f3bb9d0093a2bd
olutunde22 <solabi.tunde@gmail.com>
2024-01-17 22:14:27 +0100

    fix: form title

--- src/components/renderForm.tsx
+++ src/components/renderForm.tsx
@@ -2,8 +2,8 @@ import { Form } from '../types';
 import CustomerAuth from './forms/CustomerAuth';
 
 export const renderForm = (form: Form) => {
-  switch (form.type) {
-    case 'customer-auth':
+  switch (form.title) {
+    case 'Contact Information':
       return <CustomerAuth fields={form.fields} />;
 
     default:
